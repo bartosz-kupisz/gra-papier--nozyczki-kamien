491e2a8ee0048d37b962395ccb66bfdf9ddae15c
Bartosz Kupisz <bartosz-kupisz@wp.pl>
2021-04-19 11:40:48 +0200

    Dodanie getMoveName,displayResult

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
 if(playerInput == '1'){
   playerMove = 'kamień';
@@ -13,6 +13,37 @@ if(playerInput == '1'){
   if (playerInput == '3'){
   playerMove = 'nożyczki';
 }
+randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('Wylosowana liczba to: ' + randomNumber);
+
+getMoveName = function(argMoveId) { 
+    console.log('Wywołano funkcję getMoveName z argumentem argMoveId: ' + argMoveId);
+    if (argMoveId == 1) {
+      return 'kamień';
+    } else if (argMoveId == 2) {
+      return 'papier';
+    } else if (argMoveId == 3) {
+      return 'nożyce';
+    } else {
+      printMessage('Nie ma takiego ruchu jak "' + argMoveId + '"!');
+    }
+  }
+}
+displayResult = function(argComputerMove, argPlayerMove) { 
+  console.log('wykonano ruchy. Komputer:', argComputerMove, '. Gracz:', argPlayerMove);
+  if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == argPlayerMove) {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz!');
+  }
+}
+
 let computerMove = 'kamień';
 
 printMessage('Twój ruch to: ' + playerMove);
