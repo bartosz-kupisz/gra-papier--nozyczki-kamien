e50cde0b216f36749f6d34e5aabde82072c7f865
Bartosz Kupisz <bartosz-kupisz@wp.pl>
2021-04-22 09:24:24 +0200

    dodanie przycisków,poprawa wygladu gry

--- js/script.js
+++ js/script.js
@@ -56,3 +56,16 @@ if( computerMove == 'kamień' && playerMove == 'kamień'){
 if( computerMove == 'kamień' && playerMove == 'nożyczki'){
     printMessage('Przegrana!');
 }
+
+document.getElementById('play-rock').addEventListener('click', function() {
+  playGame(1);
+});
+
+document.getElementById('play-paper').addEventListener('click', function() {
+  playGame('2');
+});
+
+document.getElementById('play-scissors').addEventListener('click', function() {
+  playGame('3');
+});
+}
